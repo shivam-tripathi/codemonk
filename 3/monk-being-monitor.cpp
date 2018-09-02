5ab270372ea64a931314784e46f759acdc09670a
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-09-03 04:02:09 +0530

    3/monk-being-monitor: Disappointed to see wrong problem statement

--- 3/monk-being-monitor.cpp
+++ 3/monk-being-monitor.cpp
@@ -19,46 +19,36 @@ int main() {
         std::sort(heights, heights+n);
 
         // Accumulate heights
-        vector< pair<int, int> > acc;
+        vector<int> acc;
         int cur = heights[0], count = 0;
         for(int i=0; i<n; i++) {
             if (cur == heights[i]) {
                 count++;
             }
             else {
-                acc.push_back(make_pair(count, heights[i]));
+                acc.push_back(count);
                 count = 1;
                 cur = heights[i];
             }
         }
-        acc.push_back(make_pair(count, heights[n-1]));
+        acc.push_back(count);
 
-        int len = acc.size()-1;
-        int greatest_count_till_now = 0;
-        int height_with_max_count = 0;
+        if (acc.size() == 1) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int len = acc.size() - 2;
+        int greatest_count_till_now = acc[acc.size()-1];
         int maxDiff = 0;
-        bool changed = false;
-        pair<int, int> ans;
 
         while(len >= 0) {
-            if (greatest_count_till_now < acc[len].first) {
-                greatest_count_till_now = acc[len].first;
-                height_with_max_count = acc[len].second;
-            }
-            if (maxDiff < (acc[len].first - greatest_count_till_now)) {
-                changed = true;
-                maxDiff = (acc[len].first - greatest_count_till_now);
-                ans.first = height_with_max_count;
-                ans.second = acc[len].second;
-            }
+            maxDiff = max(maxDiff, greatest_count_till_now - acc[len]);
+            greatest_count_till_now = max(greatest_count_till_now, acc[len]);
             len--;
         }
 
-        if (changed) {
-            cout << maxDiff << endl;
-        } else {
-            cout << 1 << endl;
-        }
+        cout << maxDiff << endl;
     }
     return 0;
 }
