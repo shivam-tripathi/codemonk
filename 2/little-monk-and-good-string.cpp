636562e5ffbf6fa1166928d31752b3f1bc5c268b
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-09-03 01:18:46 +0530

    2/little-monk-and-good-string: Solve. Catch - don't forget the last solution

---
+++ 2/little-monk-and-good-string.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isVowel(char);
+
+bool isVowel(char c) {
+	return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+
+int main(int argc, char const *argv[])
+{
+	string s;
+	cin >> s;
+	size_t len = s.size();
+	int maxCount = 0;
+	int count = 0;
+	for(size_t i=0; i<len; i++) {
+		if (!isVowel(s[i])) {
+			maxCount = max(maxCount, count);
+			count = 0;
+		} else {
+			count++;
+		}
+	}
+	cout << max(maxCount, count) << endl;
+	return 0;
+}
